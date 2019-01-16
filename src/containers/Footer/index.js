8d0bbcb79ba8b46b60db47159c1bc239e4533f59
taha_mac <tahaouarrak@gmail.com>
2019-01-17 00:42:11 +0100

    Optimize footer

--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -2,11 +2,20 @@
 import React from 'react';
 // PROJECT
 import ProgressBar from '@/components/ProgressBar'
-
+import './footer.css'
+import largeLogo from '@/assets/img/wtjungle-large.png'
 
 const footer = (props) => (
     <div>
         <ProgressBar percent={props.percent}/>
+        <div className="footer-info">
+            <div className="footer-logo">
+                <img src={largeLogo} alt="Welcome to the jungle"/>
+            </div>
+            <div className="footer-link">
+                <a href="#">Voir le profil ></a>
+            </div>
+        </div>
     </div>
 );
 
