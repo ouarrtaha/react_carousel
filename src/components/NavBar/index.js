65f6350add8c5f7b193418eb02051e2b58155e16
taha_mac <tahaouarrak@gmail.com>
2019-01-17 00:07:34 +0100

    Add logo to navBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,9 +1,18 @@
+// NPM
 import React from 'react';
 
+// PROJECT
 import './navbar.css'
+import logo from '@/assets/img/wtjungle-logo.png'
 
 const navBar = (props) => (
     <div className="nav-bar">
+        <div className="logo">
+            <span className="logo-img">
+                <img src={logo} alt="logo"/>
+            </span>
+            <span className="welcome">Welcome to the jungle</span>
+        </div>
         <div className="btn-container">
             <button className="btn btn-previous" onClick={props.prevClicked}/>
             <button className="btn btn-next" onClick={props.nextClicked}/>
